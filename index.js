428fa89da8215212069010be7101290fec1184df
kgarcia7 <kgarcia7814@gmail.com>
2024-01-17 14:52:57 -0500

    Setting up config to db/server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express'); // importing the express.js library to create a web server
 const db = require('./config/connection'); // importing database connect config
-const routes = require('./routes'); // imports routes modules
+// const routes = require('./routes'); // imports routes modules
 
 const cwd = process.cwd(); //getting current working directory 
 
@@ -11,6 +11,9 @@ const app = express(); //creating an express app
 // middleware to parse incoming URL-encoded and JSON data
 app.use(express.urlencoded({ extended: true })); 
 app.use(express.json()); 
+
+//use routes modules
+// app.use(routes);
 
 //Once database connection is open, start express server and listen on specified port
 db.once('open', () => {

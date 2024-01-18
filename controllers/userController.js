f0a420d1b2660f355bf43aa97141bb4084a4ef9e
kgarcia7 <kgarcia7814@gmail.com>
2024-01-17 19:43:50 -0500

    Updating routes and controllers

---
+++ controllers/userController.js
@@ -0,0 +1,9 @@
+const { ObjectId } = require('mongoose').Types;
+const { getDefaultResultOrder } = require('dns');
+const { User, Thought } = require('../models');
+
+//functions needed: getUser, getSingleUser, createNewUser, updateUser, deleteUser 
+
+function getUser () {
+    
+}
---
+++ routes/api/user-routes.js
@@ -0,0 +1,36 @@
+// import express router
+const router = require('express').Router();
+
+//import functions from controller module
+const {
+    getUsers, 
+    getSingleUser, 
+    createNewUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/userController')
+
+//Defining routes
+
+// all users
+router
+    .route('/')
+    .get(getUsers)
+    .post(createNewUser);
+
+// single users
+router
+    .route('/:userID')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser)
+
+// user friends
+router
+    .route('/:userID/friends/:fiendID')
+    .post(addFriend)
+    .delete(removeFriend);
+
+    module.exports = router;
